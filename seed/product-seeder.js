2faed66929130fda54d47dd22095b050308a91e1
chillingRes <aazeez748@stu.ui.edu.ng>
2019-04-14 11:20:20 +0100

    seeding more products into database and outputting them on the index page

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -8,31 +8,12 @@ mongoose.connect("mongodb://localhost:27017/Shopping", { useNewUrlParser: true }
 var products= [
 			new Product({
 			imagePath: '/images/ipad.jpg',
-			title: 'ipad1',
-			description: 'good gadget1',
-			price: 10
+			title: 'ipad5',
+			description: 'good gadget5',
+			price: 90
 		 }),
 
-			new Product({
-			imagePath: '/images/ipad.jpg',
-			title: 'ipad2',
-			description: 'good gadget2',
-			price: 20
-		 }),
-
-			new Product({
-			imagePath: '/images/ipad.jpg',
-			title: 'ipad3',
-			description: 'good gadget3',
-			price: 10
-		 }),
-
-			new Product({
-			imagePath: '/images/ipad.jpg',
-			title: 'ipad4',
-			description: 'good gadget4',
-			price: 10
-		 }),
+			
 ];
 
 var done=0;
